51a03f0a30ed09b7b2cc627dd73325bca04e1766
Grant Ammons <grant@grant.dev>
2022-09-05 10:15:45 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import '../scss/styles.scss'
+  import { onMount } from 'svelte'
+
+  onMount(async () => {
+    const bootstrap = await import('bootstrap')
+  })
+</script>
+
+<slot />
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { sveltekit } from '@sveltejs/kit/vite'
+import type { UserConfig } from 'vite'
+import * as path from 'path'
+
+const config: UserConfig = {
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
+    }
+  }
+}
+
+export default config
