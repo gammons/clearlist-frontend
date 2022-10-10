048f5161690a108c55df3e19ebf4021df25aa729
Grant Ammons <grant@grant.dev>
2022-10-10 19:30:05 -0400

    get verify list working

--- src/routes/nav/VerifyList.svelte
+++ src/routes/nav/VerifyList.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
   let fileInput
-  let state = null
+  let uploadState = null
+  let uploadedBatch = null
+  let error = null
 
   import { get } from 'svelte/store'
 
   import ApiBackend, { backendUrl } from '../../data/backend/apiBackend'
   import { userStore, modalShowStore, batchesStore } from '../../data/stores'
   import BatchModel from '../../data/models/batch'
+
+  const backend = new ApiBackend()
 
   const closeModal = () => {
     modalShowStore.set('')
@@ -17,14 +21,27 @@
     formData.append('csv', fileInput.files[0])
     formData.append('graphql', true)
 
-    const backend = new ApiBackend()
-
     const resp = await backend.apiRequest('api/v1/batches', 'POST', $userStore.token, {}, formData)
-    console.log('resp = ', resp)
-    const batch = new BatchModel(resp)
-    console.log('batch = ', batch)
-    $batchesStore = [batch, ...$batchesStore]
-    state = 'uploaded'
+    if (resp.error) {
+      $: error = resp.error
+    } else {
+      const batch = new BatchModel(resp)
+      $: error = null
+      $: uploadState = 'uploaded'
+      $: uploadedBatch = batch
+
+      $batchesStore = [batch, ...$batchesStore]
+    }
+  }
+
+  const startBatch = async () => {
+    $: uploadState = 'starting'
+    const resp = await backend.apiRequest(
+      `api/v1/batches/${uploadedBatch.uuid}/start`,
+      'PUT',
+      $userStore.token
+    )
+    closeModal()
   }
 </script>
 
@@ -40,13 +57,26 @@
         <div class="modal-body">
           <p>here is a modal body</p>
           <p>Credits remaining: {$userStore.account.credits}</p>
-          <button
-            on:click={() => {
-              fileInput.click()
-            }}
-            type="button"
-            class="btn btn-primary">Upload CSV</button
-          >
+
+          {#if error}
+            <p>There was an error with your upload: {error}</p>
+            <p>Please try another file.</p>
+          {/if}
+
+          {#if uploadState == 'uploaded'}
+            <p>This will consume {uploadedBatch.emailCount} credits.</p>
+            <button on:click={startBatch} type="button" class="btn btn-success">Start</button>
+          {:else if uploadState == 'starting'}
+            <p>Starting processing...</p>
+          {:else}
+            <button
+              on:click={() => {
+                fileInput.click()
+              }}
+              type="button"
+              class="btn btn-primary">Upload CSV</button
+            >
+          {/if}
           <input style="display:none" type="file" on:change={uploadFile} bind:this={fileInput} />
         </div>
       </div>
